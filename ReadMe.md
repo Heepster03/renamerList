cfe473839369b4e5c3b1fbd3147019b762154d77
TeoTwawki <arawn.annwn@yandex.com>
2016-07-17 09:01:14 -0400

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+
+## How To Use This:
+
+- **Requires [Renamer](https://github.com/TeoTwawki/renamer)**
---
+++ legiondark.lua
@@ -0,0 +1,122 @@
+--[[
+    --------------------------------------------------------------------------
+    Notes:
+
+      Maximum name length is 28 characters. Don't forget the comma after each entry!
+
+    --------------------------------------------------------------------------
+]]
+
+ObjectList =
+{
+    --[[
+    [zone ID] =
+    -- EXAMPLE
+    {
+        { object ID, "name" },
+        { object ID, "name" },
+        { object ID, "name" }
+    },
+    ]]
+
+    [5] =
+    -- Uleguerand Range
+    {
+        { 16798718, "Kumhau" },
+        { 16798719, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    [7] =
+    -- Attohwa Chasm
+    {
+        { 16806910, "Achuka" },
+        { 16806911, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    [91] =
+    -- Rolanberry Fields [S]
+    {
+        { 17150970, "The Incredible Jumping Crab" },
+        { 17150971, "An Incredible Loot Box?" },
+        { 17150975, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    [110] =
+    -- Rolanberry Fields
+    {
+        { 17228395, "Legion Moogle" },
+        { 17228799, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    [111] =
+    -- Beaucedine Glacier
+    {
+        { 17232238, "Freezing Circle" },
+        { 17232894, "Umagrhk" },
+        { 17232895, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    [117] =
+    -- Tahrongi Canyon
+    {
+        { 17257470, "Hurkan" },
+        { 17257471, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    [123] =
+    -- Yuhtunga Jungle
+    {
+        { 17282046, "Darrcuiln" },
+        { 17282047, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    [159] =
+    -- Temple of Uggalepih
+    {
+        { 17429502, "Colkhab" },
+        { 17429503, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    [183] =
+    -- Maquette Abdhaljs-Legion
+    {
+        { 17527803, "Frog Lady" },
+        { 17527804, "Frog" },
+        { 17527805, "Son Goku" },
+        { 17527806, "Mars" },
+        { 17527807, "Treasures of Mars" }
+    },
+    [215] =
+    -- Abyssea - Attohwa
+    {
+        { 17658877, "MOUNTAIN WARP" },
+        { 17658879, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    [222] =
+    -- Provenance
+    {
+        { 17687540, "Minerva" },              -- Battlefield A
+        { 17687541, "Stellar Elemental" },    -- Battlefield A
+        { 17687542, "Olympian Elemental" },   -- Battlefield A
+        { 17687543, "Treasures of Minerva" }, -- Battlefield A
+        { 17687544, "Minerva" },              -- Battlefield B
+        { 17687545, "Stellar Elemental" },    -- Battlefield B
+        { 17687546, "Olympian Elemental" },   -- Battlefield B
+        { 17687547, "Treasures of Minerva" }, -- Battlefield B
+        { 17687548, "Minerva" },              -- Battlefield C
+        { 17687549, "Stellar Elemental" },    -- Battlefield C
+        { 17687550, "Olympian Elemental" },   -- Battlefield C
+        { 17687551, "Treasures of Minerva" }  -- Battlefield C
+    },
+    [243] =
+    -- Ru'Lude Gardens
+    {
+        { 17772723, "Magian Mog CJ" },
+        { 17773567, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    -- Lower Jeuno
+    {
+        { 17785850, "Limbus Mog" },
+        { 17781759, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    },
+    [255] =
+    -- Abyssea - Empyreal Paradox
+    {
+        { 17822710, "Loot Box" },
+        { 17822711, "Loot Box" },
+        { 17822712, "Loot Box" },
+        { 17822719, "PLACEHOLDER_OBJECT" } -- please report if seen in game
+    }
+};
